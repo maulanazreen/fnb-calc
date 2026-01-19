0c32e20151b410e08a2559edf4fb7a24c82d625a
Maula <maulanazreen@gmail.com>
2026-01-19 17:23:43 +0800

    removed uv headers

--- app.py
+++ app.py
@@ -1,12 +1,3 @@
-# /// script
-# requires-python = ">=3.11"
-# dependencies = [
-#   "streamlit",
-#   "pandas",
-#   "plotly",
-# ]
-# ///
-
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
